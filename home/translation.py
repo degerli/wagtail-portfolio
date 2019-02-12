7445f520ea99343647e1889f28f16385d6a91667
Raf Rasenberg <rafrasenberg@Rafs-MacBook-Pro.local>
2019-02-12 10:47:48 +0100

    Add translation option

---
+++ home/translation.py
@@ -0,0 +1,9 @@
+from .models import Foo
+from modeltranslation.translator import TranslationOptions
+from modeltranslation.decorators import register
+
+@register(Foo)
+class FooTR(TranslationOptions):
+    fields = (
+        'body',
+    )
--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -172,3 +172,11 @@ WAGTAIL_SITE_NAME = "portfolio"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+
+from django.utils.translation import gettext_lazy as _
+
+LANGUAGES = (
+    ('nl', _('Dutch')),
+    ('en', _('English')),
+)
