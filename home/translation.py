a4d9de1a63193bcae9a19f09e131fe3a0aac69cb
webconexus <raf@webconexus.nl>
2019-02-16 09:53:04 +0100

    Update translation.py

--- home/translation.py
+++ home/translation.py
@@ -1,9 +1,20 @@
 from .models import HomePage
+from projects.models import ProjectsPage, IndividualProjectsPage
 from modeltranslation.translator import TranslationOptions
 from modeltranslation.decorators import register
 
 @register(HomePage)
-class FooTR(TranslationOptions):
+class HomePageTR(TranslationOptions):
     fields = (
         'intro_left',
     )
+
+@register(ProjectsPage)
+class ProjectsPageTR(TranslationOptions)
+    fields = (
+        'description',
+    )
+
+@register(ProjectsPage)
+class IndividualProjectsPageTR(TranslationOptions)
+    pass
