3d49f0cae8d196ccd8190c62fe9462bef57c2256
webconexus <raf@webconexus.nl>
2019-02-16 10:11:19 +0100

    Add translation.py

--- home/translation.py
+++ home/translation.py
@@ -1,20 +1,12 @@
 from .models import HomePage
-from projects.models import ProjectsPage, IndividualProjectsPage
 from modeltranslation.translator import TranslationOptions
 from modeltranslation.decorators import register
 
 @register(HomePage)
 class HomePageTR(TranslationOptions):
     fields = (
+        'intro_heading',
+        'intro_sub_heading',
         'intro_left',
+        'intro_right',
     )
-
-@register(ProjectsPage)
-class ProjectsPageTR(TranslationOptions):
-    fields = (
-        'description',
-    )
-
-@register(IndividualProjectsPage)
-class IndividualProjectsPageTR(TranslationOptions):
-    pass
---
+++ projects/translation.py
@@ -0,0 +1,13 @@
+from .models import ProjectsPage, IndividualProjectsPage
+from modeltranslation.translator import TranslationOptions
+from modeltranslation.decorators import register
+
+@register(ProjectsPage)
+class ProjectsPageTR(TranslationOptions):
+    fields = (
+        'description',
+    )
+
+@register(IndividualProjectsPage)
+class IndividualProjectsPageTR(TranslationOptions):
+    pass
